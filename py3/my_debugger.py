a7d71914867fbd84db5835ce7ae9257dfa4a24be
Aquos <vaughanct@gmail.com>
2023-09-01 04:04:25 -0400

    Added Code, just general versioning commit

--- py3/my_debugger.py
+++ py3/my_debugger.py
@@ -4,3 +4,42 @@ from my_debugger_defines import *
 import sys
 import time
 kernel32 = windll.kernel32
+
+class debugger():
+
+    def __init__(self):
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = None
+        self.h_thread = None
+        self.context = None
+        self.breakpoints = {}
+        self.first_breakpoint = True
+        self.hardware_breakpoints = {}
+
+        system_info = SYSTEM_INFO()
+        kernel32.GetSystemInfo(byref(system_info))
+        self.page_size = system_info.dwPageSize
+
+        self.guarded_pages = []
+        self.memory_breakpoints = {}
+
+    def load(self, path_to_exe):
+        creation_flags = DEBUG_PROCESS
+        startupinfo = STARTUPINFO()
+        process_information = PROCESS_INFORMATION()
+
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
+
+        startupinfo.cb = sizeof(startupinfo)
+
+        if kernel32.CreateProcessA(path_to_exe, None, None, None, None, creation_flags, None, None, byref(startupinfo), byref(process_information)):
+            print("[*] We have successfully launched the process!")
+            print("[*] The Process ID I have is %d" % \
+                  process_information.dwProcessId
+            self.pid = process_information.dwProcessId)
+            self.h_process = self.open_process(self, process_information.dwProcessId)
+            self.debugger_active = True
+        else:
+            print("[*] Error with error code %d." % kernel32.GetLastError())
