6cf26cb30822cc59fc49cd2ff6f7ae084c62a91e
Aquos <vaughanct@gmail.com>
2023-09-02 04:33:05 -0400

    sERG

--- py3/my_test.py
+++ py3/my_test.py
@@ -3,3 +3,10 @@ import my_debugger
 debugger = my_debugger.debugger()
 
 debugger.load("C:\\WINDOWS\\system32\\calc.exe")
+
+#Alternate version
+#import my_debugger
+#debugger = my_debugger.debugger()
+#pid = input("Enter the PID of the process to attach to:")
+#debugger.attach(int(pid))
+#debugger.detatch()
