5ac354ef07a898ceb92a42f34829039ce0f7c88c
Aquos <vaughanct@gmail.com>
2023-09-02 04:38:43 -0400

    Coded this file as the final iteration provided in GHP SRC download

--- py3/my_test.py
+++ py3/my_test.py
@@ -1,12 +1,15 @@
+#Please see the books for the previous iterations of this file, as this file will only contain the final version included in the src file from the books website
 import my_debugger
+from my_debugger_defines import *
 
 debugger = my_debugger.debugger()
 
-debugger.load("C:\\WINDOWS\\system32\\calc.exe")
+pid = input("Enter the PID of the process to attach to: ")
 
-#Alternate version
-#import my_debugger
-#debugger = my_debugger.debugger()
-#pid = input("Enter the PID of the process to attach to:")
-#debugger.attach(int(pid))
-#debugger.detatch()
+debugger.attach(int(pid))
+
+printf = debugger.func_resolve(b"msvcrt.dll","printf")
+print("[*] Address of printf: 0x%08x" % printf)
+debugger.bp_set_mem(printf,10)
+
+debugger.run()
