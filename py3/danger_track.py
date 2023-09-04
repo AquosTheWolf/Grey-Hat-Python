886b7b58ef72be269ce0cbe4027b21321461042c
Aquos <vaughanct@gmail.com>
2023-09-03 21:37:14 -0400

    sdv

--- py3/danger_track.py
+++ py3/danger_track.py
@@ -4,3 +4,10 @@ from pydbg.defines import *
 import utils
 
 MAX_INSTRUCTIONS = 10
+
+dangerous_functions = {
+    "strcpy": "msvcrt.dll",
+    "strncpy": "msvcrt.dll",
+    "sprintf": "msvcrt.dll",
+    "vsprintf": "msvcrt.dll"
+}
